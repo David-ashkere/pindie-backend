a2e5f94dde5faf623ff52dee9fc0bf17c25d9eb8
David-ashkere <149865934+David-ashkere@users.noreply.github.com>
2024-05-23 20:18:39 +0300

    sdsfsFS

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+const cors = require("./middlewares/cors");
+const bodyParser = require("body-parser");
+const path = require("path");
+const connectToDatabase = require("./database/connect");
+
+const mainRouter = require("./routes/main");
+const usersRouter = require("./routes/users");
+const gamesRouter = require("./routes/games");
+const categoriesRouter = require("./routes/categories");
+
+const PORT = 3000;
+
+connectToDatabase();
+
+app.use(
+    cors,
+    bodyParser.json(),
+    express.static(path.join(__dirname, "/public")),
+    mainRouter,
+    usersRouter,
+    gamesRouter,
+    categoriesRouter
+)
+
+app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}`);
+})
